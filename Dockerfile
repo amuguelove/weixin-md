e8ef2561fc70cc00c55800df16efb74ad91c5a0a
Eric <908325713@qq.com>
2024-09-25 11:05:33 +0800

    [docs] adjust config

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
-FROM nginx:latest
+#FROM nginx:latest
+FROM registry.cn-chengdu.aliyuncs.com/flyeric/nginx:latest
 
 COPY dist /usr/share/nginx/html
 COPY nginx.conf /etc/nginx/conf.d/default.conf
--- deploy.sh
+++ deploy.sh
@@ -9,18 +9,13 @@ npm run build:h5-netlify
 # navigate into the build output directory
 cd dist
 
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
-
+# push build code to gh-pages
 git init
 git add -A
 
 current_time=$(date +"%Y-%m-%d %H:%M:%S")
 git commit -m "deploy on $current_time"
 
- git push -f git@github.com:flyeric0212/flyeric0212.github.io.git main
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+git push -f git@github.com:flyeric0212/flyeric0212.github.io.git main
 
 cd -
